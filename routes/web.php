243f4140c1224f8c69f28b868a57bbfce5d225c1
OngJm24 <justinmarong90@gmail.com>
2026-03-24 08:59:05 +0800

    ang desgin kag ang permissionseeder

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'public::pages.index')->name('home');
+Route::livewire('/learnmore', 'public::pages.learnmore')->name('learnmore');
 
 Route::prefix('admin')->group(function () {
     Route::livewire('/', 'admin::pages.dashboard-page')->name('admin.dashboard');
